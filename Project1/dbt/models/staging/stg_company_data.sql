f9e35ce4d8efa9a712de69819c5257c505723a0f
Sara Chiarelli <sarachiarelli@outlook.it>
2025-04-30 14:03:08 +0000

    creating staging models in dbt

--- Project1/dbt/models/staging/stg_company_data.sql
+++ Project1/dbt/models/staging/stg_company_data.sql
@@ -12,10 +12,8 @@ with companydata  as
   where cik is not null 
 )
 select
-    -- identifiers
-    {{ dbt_utils.generate_surrogate_key('cik') }} as company_id,
-
     -- company details
+    {{ dbt.safe_cast("cik", api.Column.translate_type("string")) }} as cik_company_id,
     {{ dbt.safe_cast("name", api.Column.translate_type("string")) }} as company_name,
     {{ dbt.safe_cast("ticker", api.Column.translate_type("string")) }} as company_name_shorter,
     {{ dbt.safe_cast("street1", api.Column.translate_type("string")) }} as company_address_main_street,
@@ -23,7 +21,6 @@ select
     {{ dbt.safe_cast("city", api.Column.translate_type("string")) }} as company_address_city,
     {{ dbt.safe_cast("zipCode", api.Column.translate_type("string")) }} as company_address_zipcode,
     {{ dbt.safe_cast("country", api.Column.translate_type("string")) }} as company_address_country,
-    {{ dbt.safe_cast("street1", api.Column.translate_type("string")) }} as company_address_main_street,
     {{ dbt.safe_cast("isForeignLocation", api.Column.translate_type("string")) }} as is_company_not_from_usa,
 
 from companydata
