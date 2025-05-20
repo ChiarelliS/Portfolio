78367ed338c7464f3a14b67e3a141c3e7ec2f6b8
ChiarelliS <189377243+ChiarelliS@users.noreply.github.com>
2025-05-20 13:13:30 +0000

    scaling api request and bigquery loading

--- Project2/main.py
+++ Project2/main.py
@@ -3,12 +3,41 @@ import pandas as pd
 import xmltodict
 import xml.etree.ElementTree as ET 
 import json
+import os
+import dlt
+from dlt.sources.helpers.rest_client import RESTClient
+
+
+
+# Get the secret
+os.environ["DESTINATION__BIGQUERY__CREDENTIALS"] = os.environ.get("SERVICE_ACCOUNT_KEY")
+
 
 url_catalogue = "https://ec.europa.eu/eurostat/api/dissemination/catalogue/toc/xml"
 
 file_name = 'eurostat_db.xml'
 
 dataset_name = 'Industry'
+
+query_params = {
+    "format": "JSON",
+    "lang" : "EN",
+    "indic_bt": "PROD",    # Production
+    "nace_r2": "D",        # Energy sector
+    "s_adj": "SA",  
+    "sinceTimePeriod" : "2024",
+    "geo" : "DE"
+}
+
+# define new dlt pipeline
+pipeline = dlt.pipeline(
+    pipeline_name='prod_data',
+    destination='bigquery', # <--- to run pipeline in production
+    dataset_name='eurostat_prod'
+)
+
+
+
 
 
 
@@ -37,27 +66,31 @@ def parse_xml(xmlfile, dataset_name):
                     table_codes.append(code.text)
     return(table_codes)
 
+def make_eu_prod_resource(query_params, df_code):
+    @dlt.resource(name=f"prod_{df_code}", write_disposition="replace")
+    def eu_prod():
+        client = RESTClient(base_url="https://ec.europa.eu/eurostat/")
+        endpoint = f"api/dissemination/statistics/1.0/data/{df_code}"
+        response = client.get(endpoint, params=query_params)
+        data = response.json()
+        yield data
+
+    return eu_prod
 
 
-def main():
+
+def main(url_catalogue, file_name, dataset_name):
     extract(url_catalogue, file_name)
     table_codes = parse_xml(file_name,dataset_name)
-    json_arr = []
-    for code in table_codes:
-        url_dataset = f"https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/{code}?format=JSON&lang=EN"
-        response = requests.get(url_dataset)
-        data = json.loads(response.content)
-        json_arr.append(data)
-        # with open('data.json', 'a') as outfile:
-            # json.dump(json_arr, outfile)
-    print(json_arr)
+    selected_codes = table_codes[:2]
+    for code in selected_codes:
+        resource = make_eu_prod_resource(query_params, code)
+        load_info = pipeline.run(resource, loader_file_format="parquet", write_disposition="replace")
+        print(f"Loaded dataset: {code}")
+        print(load_info)
 
 
-
-
-    
- 
    
 
 if __name__ == '__main__':
-    main()
+    main(url_catalogue,file_name,dataset_name)
